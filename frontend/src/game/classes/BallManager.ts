3f2ca37e392d43f6c422a5952c604f5d4cc2d1b2
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-07-24 00:25:15 +0530

    added sink colors

--- frontend/src/game/classes/BallManager.ts
+++ frontend/src/game/classes/BallManager.ts
@@ -1,6 +1,6 @@
 import { WIDTH, ballRadius } from "../constants";
 import { Obstacle, Sink, createObstacles, createSinks } from "../objects";
-import { pad } from "../padding";
+import { pad, unpad } from "../padding";
 import { Ball } from "./Ball";
 
 export class BallManager {
@@ -40,5 +40,40 @@ export class BallManager {
       }
     );
     this.balls.push(newBall);
+  }
+
+  drawObstacles() {
+    this.ctx.fillStyle = "white";
+    this.obstacles.forEach((obstacle) => {
+      this.ctx.beginPath();
+      this.ctx.arc(
+        unpad(obstacle.x),
+        unpad(obstacle.y),
+        obstacle.radius,
+        0,
+        Math.PI * 2
+      );
+      this.ctx.fill();
+      this.ctx.closePath();
+    });
+  }
+
+  getColor(index: number) {
+    if (index < 3 || index > this.sinks.length - 3) {
+      return { background: "#ff003f", color: "whilte" };
+    }
+    if (index < 6 || index > this.sinks.length - 6) {
+      return { background: "#ffbf00", color: "white" };
+    }
+    if (index < 9 || index > this.sinks.length - 9) {
+      return { background: "#ffbff00", color: "black" };
+    }
+    if (index < 12 || index > this.sinks.length - 12) {
+      return { background: "#ffff00", color: "black" };
+    }
+    if (index < 15 || index > this.sinks.length - 15) {
+      return { background: "#bfff00", color: "black" };
+    }
+    return { background: "#7fff00", color: "black" };
   }
 }
